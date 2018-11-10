c9c5a5d09c10528eb0fb1edeb6ee0a1f898da7f6
Jonghyun Lee <99th_day@naver.com>
2018-11-10 09:58:31 +0900

    word stemming

---
+++ skl_sentiment_analysis1.py
@@ -0,0 +1,21 @@
+import re
+import pandas as pd
+from time import time
+
+# 데이터 전처리 : 특수기호, HTML 태그 등 제거 (단, 이모티콘은 남겨둠)
+def preprocessor(text) :
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)|\^.?\^', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    return text
+
+df = pd.read_csv('/Users/paramount/Desktop/GitHub/Steam-Review/test.csv')
+
+stime = time()
+print('전처리 시작')
+df['contents'] = df['contents'].apply(preprocessor)
+print('전처리 완료: 소요시간 [%d] 초' % (time() - stime))
+df.head()
+
+
+from mylib.tokenizer import tokenizer, tokenizer_porter

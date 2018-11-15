1c2e0a9d7b928d245d1bd8207fdf222acf30b43b
Jonghyun Lee <99th_day@naver.com>
2018-11-15 10:07:38 +0900

    ing..

--- word2vec.py
+++ word2vec.py
@@ -2,26 +2,28 @@ import numpy as np
 import pandas as pd
 import nltk.tokenize
 import lda
-from collections import Counter
+# from collections import Counter
+import collections
 from nltk.stem.snowball import SnowballStemmer
 from gensim.models import Word2Vec
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics.pairwise import euclidean_distances
 
-data = pd.read_csv('C:/Users/Paramount/Desktop/GitHub/Steam-Review/data/review_temp.csv', encoding='cp949')
-data.head(5)
+data = pd.read_csv('C:/Users/Paramount/Desktop/GitHub/Steam-Review/data/reviews_removed.csv', encoding='cp949')
+data.head(3)
 
 names = set(data['kind_of_games'])
 
 names_words=[]
 for x in names:
-    names_words += [word.lower() for word in x.split()]
+    if type(x) == str:
+        names_words += [word.lower() for word in x.split()]
 
 cnt = collections.Counter()
+
 for x in names_words:
     cnt[x] += 1
-
 
 # 데이터 전처리
 
@@ -32,8 +34,9 @@ stemmer = SnowballStemmer("english", ignore_stopwords=True)
 
 stem_review = []
 for ii, words in enumerate(review):
-    print("stemmer--> ", ii, "/", len(review))
-    stem_review.append(stemmer.stem(words))
+    # print("stemmer--> ", ii, "/", len(review))
+    if type(words) == str:
+        stem_review.append(stemmer.stem(words))
 
 stem_review = np.array(stem_review)
 def eli_stop(string, stop_word, rlc = ''):
@@ -90,10 +93,11 @@ for a_line in removed_words:
 
 final_words_list = [a for i in final_words for a in i]
 
-
 # 매트릭스 생성
 # Word2Vec
 embedding_model = Word2Vec(final_words, size=100, window = 3,min_count=1)
+
+# print(embedding_model)
 
 # Word2Vec + LDA matrix
 
@@ -105,9 +109,11 @@ for ii, list_words in enumerate(final_words):
         if i in embedding_model:
             em_mat.append(embedding_model[str(i)])
             row_name.append(i)
-    print('turn-->', ii, "/", len(final_words))
+    # print('turn-->', ii, "/", len(final_words))
 
 mat_list = pd.DataFrame(em_mat,index=[row_name])
+
+# print(len(row_name))
 
 # mat_list = pd.read_csv(r"C:\Users\User\PycharmProjects\word2vec\word2vec_word.csv",encoding='cp949')
 # mat_list.head(3)
@@ -117,10 +123,12 @@ count = countvec.fit_transform(final_words_list)
 topic_vocab=countvec.get_feature_names()
 titles = names
 
+
 # LDA modeling
 model = lda.LDA(n_topics=12, n_iter=500, random_state=6, alpha=0.1)
 model.fit(count)
 topic_word = np.array(topic_vocab)[np.argsort(model.topic_word_)][:,:-1]
+
 
 # lda percent
 word_topic = np.array(np.argsort(model.topic_word_))[:,::-1]
@@ -130,18 +138,17 @@ np.shape(word_topic)
 # combine model - topic vector --------------------
 lda_mat = []
 for ii, wor in enumerate(topic_word):
-    print("turn--> ", ii, '/', len(topic_word))
+    # print("turn--> ", ii, '/', len(topic_word))
     wor_vec = []
     for wo in wor:
         wor_vec.append(embedding_model[wo])
     lda_mat.append(wor_vec)
 
-
 topic_range=range(12)
 
 theta_i = []
 for n, i in enumerate(topic_range):
-    print("turn--> ", n, "/", len(topic_range))
+    # print("turn--> ", n, "/", len(topic_range))
     topic_theta = [topic/sum(word_topic[i]) for topic in word_topic[i]]
     theta_i.append(topic_theta)
 
@@ -166,12 +173,11 @@ review_data = pd.DataFrame(review_data)
 
 total_docu_vec = []
 for n, docu in enumerate(final_words):
-    print('turn--> ', n, '/', len(final_words))
+    # print('turn--> ', n, '/', len(final_words))
     docu_vector = []
     for wo in docu:
         docu_vector.append(embedding_model[wo])
     total_docu_vec.append(docu_vector)
-
 
 game_name = list(names)
 game = np.array(data['kind_of_games'])
@@ -184,7 +190,6 @@ for n, na in enumerate(game_name):
         if na == game[ga]:
             what_game.append(total_docu_vec[ga])
     game_re_vec.append(what_game)
-
 
 sum_mat = []
 for line in range(len(game_re_vec)):
@@ -207,7 +212,7 @@ for p in range(len(sum_mat)):
     bb = [a for i in dist for a in i]
     dist_mat.append([a for i in bb for a in i])
 
-# print(dist_mat)
+print(dist_mat)
 
 # select topic ----------------------------------------
 selected_topic = [np.argsort(dist_mat[n])[:5] for n in range(len(dist_mat))]
